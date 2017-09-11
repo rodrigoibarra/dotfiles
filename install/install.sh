2acf4dc056ba72a76b6e7b817ac18414235fc9be
rodrigoibarra <hola@rodrigo-ibarra.com>
2017-09-11 14:30:32 -0500

    moved everything to install/install.sh to have everything up on a fresh installation

--- install/install.sh
+++ install/install.sh
@@ -14,6 +14,9 @@ brew install openssl
 brew install nmap
 brew install git
 brew install node
+brew install ruby
+brew install reattach-to-user-namespace
+brew install hub
 brew install python3
 brew install thefuck
 brew install wget
@@ -39,7 +42,7 @@ brew install mas
 
 brew cask install --appdir="/Applications" google-chrome
 brew cask install --appdir="/Applications" dropbox
-brew cask install --appdir="/Applications" google-drive
+brew cask install --appdir="/Applications" google-backup-and-sync
 brew cask install --appdir="/Applications" slack
 brew cask install --appdir="/Applications" atom
 brew cask install --appdir="/Applications" phpstorm
@@ -50,6 +53,8 @@ brew cask install --appdir="/Applications" the-unarchiver
 brew cask install --appdir="/Applications" iterm2
 brew cask install --appdir="/Applications" fork
 brew cask install --appdir="/Applications" sketch
+brew cask install --appdir="/Applications" openemu
+brew cask install --appdir="/Applications" firefox
 brew cask install iina
 
 # Quick Look Plugins (https://github.com/sindresorhus/quick-look-plugins)
@@ -60,3 +65,44 @@ sleep 1
 
 # ...and then.
 echo "Success! Brew additional applications are installed."
+
+echo "Installing everything else"
+
+# set up ruby
+
+
+# shellcheck disable=SC1090
+source "$HOME/.asdf/asdf.sh"
+install_asdf_plugin "ruby" "https://github.com/asdf-vm/asdf-ruby.git"
+install_asdf_plugin "nodejs" "https://github.com/asdf-vm/asdf-nodejs.git"
+
+install_asdf_language() {
+  local language="$1"
+  local version
+  version="$(asdf list-all "$language" | tail -1)"
+
+  if ! asdf list "$language" | grep -Fq "$version"; then
+    asdf install "$language" "$version"
+    asdf global "$language" "$version"
+  fi
+}
+
+fancy_echo "Installing latest Ruby..."
+install_asdf_language "ruby"
+gem update --system
+gem_install_or_update "bundler"
+number_of_cores=$(sysctl -n hw.ncpu)
+bundle config --global jobs $((number_of_cores - 1))
+
+# This installs Jekyll
+gem install jekyll
+
+# Now Grunt
+
+npm install -g grunt-cli
+
+# Install oh my zsh
+curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+
+# Make zsh default shell
+sudo chsh -s $(which zsh)
