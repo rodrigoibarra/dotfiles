3a5478efe57fa311f207bc8b89d6f62ab30f7001
rodrigoibarra <hola@rodrigo-ibarra.com>
2024-07-26 11:01:36 -0600

    Creates config for a new enviroment
    This commit adds configuration for my new env, switching terminal
    emulators to alacritty, prompt to starship, with mostly the same theme
    across everything, missing is: linking vs code config to the dotfiles
    and a new clean install script to reflect all of this.

--- cleanup.sh
+++ cleanup.sh
@@ -6,26 +6,26 @@ BASEDIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "${BASEDIR}"
 
 
+ rm -rf $HOME/.gitconfig
+ ln -s ${BASEDIR}/dotfiles/gitconfig $HOME/.gitconfig
 
-# Removes .zshrc from $HOME (if it exists) and symlinks the .zshrc file from the .dotfiles
-rm -rf $HOME/.bash_profile
-ln -s ${BASEDIR}/dotfiles/bash_profile $HOME/.bash_profile
-
-rm -rf $HOME/.bashrc
-ln -s ${BASEDIR}/dotfiles/bashrc $HOME/.bashrc
-
-rm -rf $HOME/.gitconfig
-ln -s ${BASEDIR}/dotfiles/gitconfig $HOME/.gitconfig
-
-rm -rf $HOME/.gitignore_global
-ln -s ${BASEDIR}/dotfiles/gitignore_global $HOME/.gitignore_global
-
-rm -rf $HOME/.profile
-ln -s ${BASEDIR}/dotfiles/profile $HOME/.profile
+ rm -rf $HOME/.gitignore_global
+ ln -s ${BASEDIR}/dotfiles/gitignore_global $HOME/.gitignore_global
 
 rm -rf $HOME/.zshrc
 ln -s ${BASEDIR}/dotfiles/zshrc $HOME/.zshrc
 
-rm -rf $HOME/.hyper.js
-ln -s ${BASEDIR}/dotfiles/hyper.js $HOME/.hyper.js
+rm -rf $HOME/.config/alacritty/alacritty.toml
+ln -s ${BASEDIR}/dotfiles/alacritty/alacritty.toml $HOME/.config/alacritty/alacritty.toml
 
+rm -rf $HOME/.config/alacritty/catppuccin-mocha.toml
+ln -s ${BASEDIR}/dotfiles/alacritty/catppuccin-mocha.toml $HOME/.config/alacritty/catppuccin-mocha.toml
+
+rm -rf $HOME/.config/starship.toml
+ln -s ${BASEDIR}/dotfiles/starship.toml $HOME/.config/starship.toml
+
+rm -rf $HOME/.config/bat/config
+ln -s ${BASEDIR}/dotfiles/bat/config $HOME/.config/bat/config
+
+rm -rf $HOME/.config/bat/themes/Catppuccin Mocha.tmTheme
+ln -s ${BASEDIR}/dotfiles/bat/themes/Catppuccin-Mocha.tmTheme $HOME/.config/bat/themes/Catppuccin-Mocha.tmTheme
