cf1513ee4208818c60624c780b691f28d70d9f70
Rodrigo Ibarra <rodrigoibarra@Rodrigos-MacBook-Pro.local>
2025-08-21 18:00:23 -0600

    this commit cleans up my nvim config
    There was a bunch of stuff i wasnt using, and that I really didnt
    understand, so I got rid of it. got rid of mason too, and just went for
    a normal nvim lsp config set up.

--- nvim/.config/nvim/lua/bunnz/core/options.lua
+++ nvim/.config/nvim/lua/bunnz/core/options.lua
@@ -1,32 +1,29 @@
-vim.cmd("let g:netrw_liststyle=3")
-
-local opt= vim.opt
+local opt = vim.opt
 
 opt.relativenumber = true
 opt.number = true
 
 -- tabs & indentation
- opt.tabstop = 2
- opt.shiftwidth = 2
- opt.expandtab = true
- opt.autoindent = true
+opt.tabstop = 4
+opt.shiftwidth = 4
+opt.expandtab = true
+opt.autoindent = true
+opt.wrap = false
 
- opt.wrap = false
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
 
- -- search settings
- opt.ignorecase = true
- opt.smartcase = true
+opt.cursorline = true
 
- opt.cursorline = true
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
 
- opt.termguicolors = true
- opt.background = "dark"
- opt.signcolumn = "yes"
+opt.backspace = "indent,eol,start"
 
- opt.backspace = "indent,eol,start"
+opt.clipboard:append("unnamedplus")
 
- opt.clipboard:append("unnamedplus")
-
- --splitting
- opt.splitright = true
- opt.splitbelow = true
+--splitting
+opt.splitright = true
+opt.splitbelow = true
--- nvim/.config/nvim/lua/bunnz/plugins/nvim-tree.lua
+++ nvim/.config/nvim/lua/bunnz/plugins/nvim-tree.lua
@@ -1,50 +1,49 @@
 return {
-
     "nvim-tree/nvim-tree.lua",
-  version = "*",
-  lazy = false,
-  dependencies = {
-    "nvim-tree/nvim-web-devicons",
-  },
-  config = function()
-    
-    vim.g.loaded_netrw = 1
-    vim.g.loaded_netrwPlugin = 1
-
-    require("nvim-tree").setup {
-      view = {
-        width = 35,
-        relativenumber = true,
-      },
-      -- change folder arrow icons
-      renderer = {
-        indent_markers = {
-          enable = true,
-        },
-        icons = {
-          glyphs = {
-            folder = {
-              arrow_closed = "", -- arrow when folder is closed
-              arrow_open = "", -- arrow when folder is open
+    version = "*",
+    lazy = false,
+    dependencies = {
+        "nvim-tree/nvim-web-devicons",
+    },
+    config = function()
+        local nvimtree = require("nvim-tree")
+        -- disable netrw at the very start of your init.lua
+        vim.g.loaded_netrw = 1
+        vim.g.loaded_netrwPlugin = 1
+        require("nvim-tree").setup({
+            sort = {
+                sorter = "case_sensitive",
             },
-          },
-        },
-      },
+            view = {
+                width = 30,
+            },
+            renderer = {
+                group_empty = true,
+            },
             filters = {
-        custom = { ".DS_Store" },
-      },
-      git = {
-        ignore = false,
-      }
-    }
+                exclude = {
+                    "node_modules",
+                    ".git",
+                    "build",
+                },
+                custom = {
+                    ".DS_store",
 
-    local keymap = vim.keymap -- for conciseness
+                },
 
-    keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" }) -- toggle file explorer
-    keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>", { desc = "Toggle file explorer on current file" }) -- toggle file explorer on current file
-    keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
-    keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" }) -- refresh file explorer
 
-  end,
+            }
+        })
 
+        -- set keymaps
+        local keymap = vim
+        .keymap                                                                                         -- for conciseness
+
+        keymap.set("n", "<leader>ee", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })     -- toggle file explorer
+        keymap.set("n", "<leader>eq", "<cmd>NvimTreeToggle<CR>", { desc = "Toggle file explorer" })     -- this is just because I like to use different bindings to open and close
+        keymap.set("n", "<leader>ef", "<cmd>NvimTreeFindFileToggle<CR>",
+            { desc = "Toggle file explorer on current file" })                                          -- toggle file explorer on current file
+        keymap.set("n", "<leader>ec", "<cmd>NvimTreeCollapse<CR>", { desc = "Collapse file explorer" }) -- collapse file explorer
+        keymap.set("n", "<leader>er", "<cmd>NvimTreeRefresh<CR>", { desc = "Refresh file explorer" })   -- refresh file explorer
+    end,
 }
