61a055c01d20837b4fbc476c107fbfafe4758f9f
Rodrigo Ibarra <rodrigoibarra@Rodrigos-MacBook-Pro.local>
2026-04-18 16:54:03 -0600

    feat(nvim): add mini.surround and mini.indentscope plugins
    - Add mini.surround for wrapping selections with brackets, quotes, etc.
    - Add mini.indentscope for indentation scope visualization and text
    objects

---
+++ nvim/.config/nvim/lua/bunnz/plugins/mini.indentscope.lua
@@ -0,0 +1,11 @@
+-- lua/plugins/mini-indentscope.lua
+return {
+	"nvim-mini/mini.indentscope",
+	version = "*",
+	opts = {
+		symbol = "│",
+		options = {
+			try_as_border = true,
+		},
+	},
+}
---
+++ nvim/.config/nvim/lua/bunnz/plugins/mini.surround.lua
@@ -0,0 +1,16 @@
+-- lua/plugins/mini-surround.lua
+return {
+	"nvim-mini/mini.surround",
+	version = "*",
+	opts = {
+		mappings = {
+			add = "sa", -- Add surrounding in Normal and Visual modes
+			delete = "sd", -- Delete surrounding
+			find = "sf", -- Find surrounding (to the right)
+			find_left = "sF", -- Find surrounding (to the left)
+			highlight = "sh", -- Highlight surrounding
+			replace = "sr", -- Replace surrounding
+			update_n_lines = "sn", -- Update `n_lines`
+		},
+	},
+}
